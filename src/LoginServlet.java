89b76c2d16726af6e39c2f3fb133a36f6ac8e07a
JoseRuv <joserru030@gmail.com>
2023-10-17 15:37:39 -0700

    Added query that verifies user info initially.

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/api/login")
 public class LoginServlet extends HttpServlet {
@@ -11,12 +13,34 @@ public class LoginServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
+        String username = request.getParameter("Username");
+        String password = request.getParameter("Password");
 
         /* This example only allows username/password to be test/test
         /  in the real project, you should talk to the database to verify username/password
         */
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Construct a query with parameter represented by "?" where it returns the users info if it exists in the customer table
+        String queryUserInput = "SELECT *" +
+                " FROM customers" +
+                " WHERE customers.email = ?" +
+                "AND customers.password = ?;";
+
+        // Declare our statement
+        PreparedStatement statementUserInput = conn.prepareStatement(queryUserInput);
+
+        // Set the parameter represented by "?" in the query to the username and password we get from the form,
+        // num 1 indicates the first "?" in the query
+        statementUserInput.setString(1, username);
+        statementUserInput.setString(2, password);
+
+        // Perform the query
+        ResultSet resultSetStarInfo = statementUserInput.executeQuery();
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
         JsonObject responseJsonObject = new JsonObject();
         if (username.equals("anteater") && password.equals("123456")) {
             // Login success:
@@ -34,9 +58,9 @@ public class LoginServlet extends HttpServlet {
             request.getServletContext().log("Login failed");
             // sample error messages. in practice, it is not a good idea to tell user which one is incorrect/not exist.
             if (!username.equals("anteater")) {
-                responseJsonObject.addProperty("message", "user " + username + " doesn't exist");
+                responseJsonObject.addProperty("message", "User " + username + " doesn't exist");
             } else {
-                responseJsonObject.addProperty("message", "incorrect password");
+                responseJsonObject.addProperty("message", "Incorrect password");
             }
         }
         response.getWriter().write(responseJsonObject.toString());
