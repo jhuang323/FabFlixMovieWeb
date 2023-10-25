47b2340862141fdee276a074db7cdf255b109e40
Justin Huang <huangjk2@uci.edu>
2023-10-24 19:06:52 -0700

    Implement post form for checkout

---
+++ src/CheckoutServlet.java
@@ -0,0 +1,79 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * This IndexServlet is declared in the web annotation below,
+ * which is mapped to the URL pattern /api/index.
+ */
+@WebServlet(name = "CheckoutServlet", urlPatterns = "/api/checkout")
+public class CheckoutServlet extends HttpServlet {
+
+    /**
+     * handles GET requests to store session information
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
+        String sessionId = session.getId();
+        long lastAccessTime = session.getLastAccessedTime();
+
+        JsonObject responseJsonObject = new JsonObject();
+        responseJsonObject.addProperty("sessionID", sessionId);
+        responseJsonObject.addProperty("lastAccessTime", new Date(lastAccessTime).toString());
+
+        ArrayList<String> previousItems = (ArrayList<String>) session.getAttribute("previousItems");
+        if (previousItems == null) {
+            previousItems = new ArrayList<String>();
+        }
+        // Log to localhost log
+        request.getServletContext().log("getting " + previousItems.size() + " items");
+        JsonArray previousItemsJsonArray = new JsonArray();
+        previousItems.forEach(previousItemsJsonArray::add);
+        responseJsonObject.add("previousItems", previousItemsJsonArray);
+
+        // write all the data into the jsonObject
+        response.getWriter().write(responseJsonObject.toString());
+    }
+
+    /**
+     * handles POST requests to add and show the item list information
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String firstName = request.getParameter("First_name");
+        String lastName = request.getParameter("Last_name");
+        String creditCardNumber = request.getParameter("CreditCard_number");
+        String expirationDate = request.getParameter("Expiration_date");
+
+        HttpSession session = request.getSession();
+
+//        // get the previous items in a ArrayList
+//        ArrayList<String> previousItems = (ArrayList<String>) session.getAttribute("previousItems");
+//        if (previousItems == null) {
+//            previousItems = new ArrayList<String>();
+//            previousItems.add(item);
+//            session.setAttribute("previousItems", previousItems);
+//        } else {
+//            // prevent corrupted states through sharing under multi-threads
+//            // will only be executed by one thread at a time
+//            synchronized (previousItems) {
+//                previousItems.add(item);
+//            }
+//        }
+//
+//        JsonObject responseJsonObject = new JsonObject();
+//
+//        JsonArray previousItemsJsonArray = new JsonArray();
+//        previousItems.forEach(previousItemsJsonArray::add);
+//        responseJsonObject.add("previousItems", previousItemsJsonArray);
+//
+//        response.getWriter().write(responseJsonObject.toString());
+    }
+}
