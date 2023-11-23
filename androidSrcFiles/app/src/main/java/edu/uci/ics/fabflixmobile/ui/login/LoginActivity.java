168296584487f545bab02c2a3cff605c695f6f40
Justin Huang <huangjk2@uci.edu>
2023-11-22 18:48:32 -0800

    Added android src file

---
+++ androidSrcFiles/app/src/main/java/edu/uci/ics/fabflixmobile/ui/login/LoginActivity.java
@@ -0,0 +1,112 @@
+package edu.uci.ics.fabflixmobile.ui.login;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import edu.uci.ics.fabflixmobile.data.NetworkManager;
+import edu.uci.ics.fabflixmobile.databinding.ActivityLoginBinding;
+import edu.uci.ics.fabflixmobile.ui.mainpage.MainPageActivity;
+import edu.uci.ics.fabflixmobile.ui.movielist.MovieListActivity;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private TextView message;
+
+    /*
+      In Android, localhost is the address of the device or the emulator.
+      To connect to your machine, you need to use the below IP address 10.0.2.2
+     */
+    private final String host = "54.176.122.219";
+    private final String port = "8443";
+    private final String domain = "cs122b-project4";
+    private final String baseURL = "https://" + host + ":" + port + "/" + domain;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        // upon creation, inflate and initialize the layout
+        setContentView(binding.getRoot());
+
+        username = binding.username;
+        password = binding.password;
+        message = binding.message;
+        final Button loginButton = binding.login;
+
+        //assign a listener to call a function to handle the user request when clicking a button
+        loginButton.setOnClickListener(view -> login());
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void login() {
+        message.setText("Trying to login");
+        // use the same network queue across our application
+        final RequestQueue queue = NetworkManager.sharedManager(this).queue;
+        // request type is POST
+        final StringRequest loginRequest = new StringRequest(
+                Request.Method.POST,
+                baseURL + "/api/android_login",
+                response -> {
+                    // TODO: should parse the json response to redirect to appropriate functions
+                    //  upon different response value.
+                    //json.parse, show "login failed" if json returns false
+                    //check message and display it
+                    //then ask if status is success or fail
+                    try {
+                        JSONObject jsonobject = new JSONObject(response);
+                        String success = jsonobject.getString("status");
+                        if(success.equals("success")){
+                            Log.d("login.success", response);
+                            message.setText("Success");
+                            finish();
+                            // initialize the activity(page)/destination
+                            Intent MovieListPage = new Intent(LoginActivity.this, MainPageActivity.class);
+
+                            MovieListPage.putExtra("host",host);
+                            MovieListPage.putExtra("port",port);
+                            MovieListPage.putExtra("domain",domain);
+
+                            // activate the list page.
+                            startActivity(MovieListPage);
+                        }
+                        else{
+                            message.setText(jsonobject.getString("message"));
+                        }
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                    //Complete and destroy login activity once successful
+                },
+                error -> {
+                    // error
+                    Log.d("login.error", error.toString());
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                // POST request form data
+                final Map<String, String> params = new HashMap<>();
+                params.put("username", username.getText().toString());
+                params.put("password", password.getText().toString());
+                return params;
+            }
+        };
+        // important: queue.add is where the login request is actually sent
+        queue.add(loginRequest);
+    }
+}
